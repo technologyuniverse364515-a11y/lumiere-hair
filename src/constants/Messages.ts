f491899630682aeb7dc5bc1569d6acc9095e3487
Technology Universe <technologyuniverse364515@gmail.com>
2026-05-06 23:57:51 +0530

    Updates

--- src/constants/Messages.ts
+++
@@ -1,7 +0,0 @@
-export const Messages = {
-  Contact: {
-    success: "Message sent successfully! We'll be in touch shortly.",
-    error: "Something went wrong. Please try again.",
-    validation: "Please fill in all required fields.",
-  },
-};
